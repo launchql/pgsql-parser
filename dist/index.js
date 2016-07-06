7c74b0776008014ce813334f4dc663920396d197
Zac McCormick <zac.mccormick@gmail.com>
2016-07-06 10:25:11 -0400

    Add verify method

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Deparser = exports.clean = exports.byType = exports.tables = exports.all = exports.first = exports.walk = exports.deparse = exports.parse = undefined;
+exports.Deparser = exports.verify = exports.clean = exports.byType = exports.tables = exports.all = exports.first = exports.walk = exports.deparse = exports.parse = undefined;
 
 var _pgQueryNative = require('pg-query-native');
 
@@ -17,6 +17,15 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const deparse = _deparser2.default.deparse;
 
+const verify = query => {
+  const result = deparse((0, _pgQueryNative.parse)(query).query);
+
+  const json1 = (0, _utils.clean)((0, _pgQueryNative.parse)(query).query);
+  const json2 = (0, _utils.clean)((0, _pgQueryNative.parse)(result).query);
+
+  return JSON.stringify(json1) === JSON.stringify(json2);
+};
+
 exports.parse = _pgQueryNative.parse;
 exports.deparse = deparse;
 exports.walk = _utils.walk;
@@ -25,5 +34,6 @@ exports.all = _utils.all;
 exports.tables = _utils.tables;
 exports.byType = _utils.byType;
 exports.clean = _utils.clean;
+exports.verify = verify;
 exports.Deparser = _deparser2.default;
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ import { walk, all, first, tables, byType, clean } from './utils';
 
 const deparse = Deparser.deparse;
 
-export { parse, deparse, walk, first, all, tables, byType, clean, Deparser };
+const verify = (query) => {
+  const result = deparse(parse(query).query);
+
+  const json1 = clean(parse(query).query);
+  const json2 = clean(parse(result).query);
+
+  return JSON.stringify(json1) === JSON.stringify(json2);
+};
+
+export { parse, deparse, walk, first, all, tables, byType, clean, verify, Deparser };
