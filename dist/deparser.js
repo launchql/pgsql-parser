76fdbe61faf4459a46fc8d88b03f95a46f00de61
Dan Lynch <pyramation@gmail.com>
2018-08-19 15:53:44 -0700

    generated

--- dist/deparser.js
+++ dist/deparser.js
@@ -808,6 +808,11 @@ class Deparser {
       output.push(')');
     }
 
+    if (node.whereClause) {
+      output.push('WHERE');
+      output.push(this.deparse(node.whereClause));
+    }
+
     return output.join(' ');
   }
 
