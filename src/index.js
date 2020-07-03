e8d8c7659c7d428def361b6c67e6c01541433217
Dan Lynch <pyramation@gmail.com>
2020-07-03 04:06:56 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 import Deparser from './deparser';
-import { walk, all, first, tables, byType, clean } from './utils';
-const parse = require('pg-plpgsql-query-native').parseQuerySync;
+const parse = require('@pgql/parse').parseQuerySync;
 const deparse = Deparser.deparse;
-const verify = query => {
-  const result = deparse(parse(query));
-
-  const json1 = clean(parse(query));
-  const json2 = clean(parse(result));
-
-  return JSON.stringify(json1) === JSON.stringify(json2);
-};
-
-export {
-  parse,
-  deparse,
-  walk,
-  first,
-  all,
-  tables,
-  byType,
-  clean,
-  verify,
-  Deparser
-};
+export { parse, deparse, Deparser };
