512c9e3b33d49d32fbd84785ffffe09460288d09
Dan Lynch <pyramation@gmail.com>
2020-10-23 00:07:16 -0700

    cases

--- __fixtures__/grants/custom.sql
+++ __fixtures__/grants/custom.sql
@@ -7,6 +7,10 @@ REVOKE EXECUTE ON FUNCTION auth.authenticate FROM anonymous;
 REVOKE SELECT,INSERT,UPDATE,DELETE ON TABLE auth.token FROM administrator;
 
 GRANT SELECT, INSERT ON someschema.sometable2 TO somerole; 
+
 GRANT UPDATE (col2) ON someschema.sometable2 TO somerole;
 GRANT UPDATE (col2,col3) ON someschema.sometable2 TO somerole;
 GRANT UPDATE (col2,"another-column") ON someschema.sometable2 TO somerole;
+GRANT INSERT (col2), UPDATE (col2,"another-column"), DELETE ON someschema.sometable2 TO somerole;
+GRANT INSERT (col2,col3) ON someschema.sometable2 TO somerole;
+GRANT INSERT (col2,"another-column") ON someschema.sometable2 TO somerole;
