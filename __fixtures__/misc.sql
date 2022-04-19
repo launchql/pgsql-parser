5b85fb16ac2c3f70f37d8ba3542f7f9469839f8f
Dan Lynch <pyramation@gmail.com>
2022-04-19 10:03:10 -0700

    test case closes #49

--- __fixtures__/misc.sql
+++ __fixtures__/misc.sql
@@ -3,3 +3,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql;
 
 CREATE INDEX index_email_logs_on_created_at ON public.email_logs USING btree (created_at DESC);
 CREATE INDEX index_email_logs_on_created_at ON public.email_logs USING btree (created_at DESC, another_prop ASC);
+
+
+ALTER TABLE "Customer" ADD CONSTRAINT myconstraint
+  FOREIGN KEY ("SupportRepId") REFERENCES "Employee" ("EmployeeId") ON DELETE NO ACTION ON UPDATE NO ACTION;
