ec1110a671ce56b1be84937d123c3c059e42e50b
Andrew Carlson <2yinyang2@gmail.com>
2022-08-30 22:14:49 -0400

    refactor shared code between sync and async

--- packages/parser/src/index.js
+++ packages/parser/src/index.js
@@ -6,32 +6,26 @@ import {
   parsePlPgSQLSync as parseFunction
 } from 'libpg-query';
 
+function mapStmt({ stmt, stmt_len, stmt_location }) {
+  return {
+    RawStmt: {
+      stmt,
+      stmt_len,
+      stmt_location: stmt_location || 0
+    }
+  };
+}
+
 export const parse = (sql) => {
   if (!sql) throw new Error('no SQL provided to parser');
   const result = parseQuerySync(sql);
-  return result.stmts.map(({ stmt, stmt_len, stmt_location }) => {
-    return {
-      RawStmt: {
-        stmt,
-        stmt_len,
-        stmt_location: stmt_location || 0
-      }
-    };
-  });
+  return result.stmts.map(mapStmt);
 };
 
 export const parseAsync = async (sql) => {
   if (!sql) throw new Error('no SQL provided to parser');
   const result = await parseQuery(sql);
-  return result.stmts.map(({ stmt, stmt_len, stmt_location }) => {
-    return {
-      RawStmt: {
-        stmt,
-        stmt_len,
-        stmt_location: stmt_location || 0
-      }
-    };
-  });
+  return result.stmts.map(mapStmt);
 };
 
 export { deparse, Deparser, enums, parseFunction };
