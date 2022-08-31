0ba7ea66283d6f2be3f41991c2d8395d92f49051
Andrew Carlson <2yinyang2@gmail.com>
2022-08-30 22:11:11 -0400

    add async parser function parseAsync

--- packages/parser/src/index.js
+++ packages/parser/src/index.js
@@ -1,10 +1,28 @@
 import { Deparser, deparse } from 'pgsql-deparser';
 import { enums } from 'pgsql-enums';
-import { parseQuerySync, parsePlPgSQLSync as parseFunction } from 'libpg-query';
+import {
+  parseQuery,
+  parseQuerySync,
+  parsePlPgSQLSync as parseFunction
+} from 'libpg-query';
 
 export const parse = (sql) => {
   if (!sql) throw new Error('no SQL provided to parser');
   const result = parseQuerySync(sql);
+  return result.stmts.map(({ stmt, stmt_len, stmt_location }) => {
+    return {
+      RawStmt: {
+        stmt,
+        stmt_len,
+        stmt_location: stmt_location || 0
+      }
+    };
+  });
+};
+
+export const parseAsync = async (sql) => {
+  if (!sql) throw new Error('no SQL provided to parser');
+  const result = await parseQuery(sql);
   return result.stmts.map(({ stmt, stmt_len, stmt_location }) => {
     return {
       RawStmt: {
