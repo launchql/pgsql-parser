9625c83603b8fff4f3b66a214159360b4625b535
Dan Lynch <pyramation@gmail.com>
2024-03-30 00:49:32 -0700

    typescript, jest, and tests

--- packages/enums/__test__/enums.test.ts
+++ packages/enums/__test__/enums.test.ts
@@ -1,6 +1,7 @@
+import { describe, expect, test } from '@jest/globals';
 import { getEnum } from '../src';
 
-it('getter', () => {
+test('getter', () => {
   expect(getEnum('OverridingKind', 0)).toEqual('OVERRIDING_NOT_SET');
   expect(getEnum('OverridingKind', 'OVERRIDING_NOT_SET')).toEqual(0);
 });
--- packages/enums/types/getters.d.ts
+++
@@ -1,1 +0,0 @@
-export function getAstNodes(): any;
