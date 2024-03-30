7116edb93c471521ee7eaad962c36f1e5fed4d1f
Dan Lynch <pyramation@gmail.com>
2024-03-29 20:06:33 -0700

    types

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,12 +5,11 @@ export default async (argv) => {
   if (!argv.inputFile || !argv.outputDir) {
     console.log('inputFile and outputDir are required!');
     console.log('Usage:');
-    console.log('pg-proto-parser --inputFile=input.proto --outputDir=out');
+    console.log('pg-proto-parser --inputFile input.proto --outputDir out');
     process.exit(1);
   }
 
-
-  const parser = new PgProtoParser(argv.inputFIle, argv.outputDir);
+  const parser = new PgProtoParser(argv.inputFile, argv.outputDir);
 
   // Generate TypeScript and JSON files
   await parser.write();
