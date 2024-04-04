7951d7248683a23ee7782b8ff7f204726d0c7646
Dan Lynch <pyramation@gmail.com>
2024-04-03 19:55:05 -0700

    types

--- packages/parser/types/index.d.ts
+++ packages/parser/types/index.d.ts
@@ -1,5 +1,5 @@
-import { Deparser, deparse } from 'pgsql-deparser';
+import { Node } from '@pgsql/types';
 import { parsePlPgSQLSync as parseFunction } from 'libpg-query';
-export declare const parse: (sql: any) => any;
+export declare const parse: (sql: any) => Node;
 export declare const parseAsync: (sql: any) => Promise<any>;
-export { deparse, Deparser, parseFunction };
+export { parseFunction };
