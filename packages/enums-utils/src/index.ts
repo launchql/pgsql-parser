a82a1d753d78372779cea1cfd5646427bb65a348
Dan Lynch <pyramation@gmail.com>
2025-06-03 14:04:54 -0700

    initialized packaegs

--- packages/enums-utils/src/index.ts
+++ packages/enums-utils/src/index.ts
@@ -1,3 +1,14 @@
-export default () => {
+import toInt from './enums2int.json';
+import toStr from './enums2str.json';
+import nodes from './nodes.json';
 
+export { toInt, toStr, nodes };
+export const getEnum = (enumType, key) => {
+  if (typeof key === 'number') {
+    return toStr[enumType][key];
+  } else {
+    return toInt[enumType][key];
+  }
 };
+
+export * from './types';
