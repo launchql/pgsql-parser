287283bc94df7aa378cb0f6864ea59f20d1577c4
Dan Lynch <pyramation@gmail.com>
2025-06-30 21:32:29 -0700

    pretty

--- packages/deparser/__tests__/pretty/alter-table-column.test.ts
+++ packages/deparser/__tests__/pretty/alter-table-column.test.ts
@@ -1,6 +1,14 @@
 import { PrettyTest } from '../../test-utils/PrettyTest';
 const prettyTest = new PrettyTest([
-    "original/alter/alter-table-column-1.sql"
+    "original/alter/alter-table-column-1.sql",
+    "original/alter/alter-table-column-2.sql",
+    "original/alter/alter-table-column-3.sql",
+    "original/alter/alter-table-column-4.sql",
+    "original/alter/alter-table-column-5.sql",
+    "original/alter/alter-table-column-6.sql",
+    "original/alter/alter-table-column-7.sql",
+    "original/alter/alter-table-column-8.sql",
+    "original/alter/alter-table-column-9.sql"
 ]);
 
 prettyTest.generateTests();
